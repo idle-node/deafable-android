4d001f1f59bcdcc7ac05c3d0b10a3526757e0fe3
Moanrisy <moanrisy@gmail.com>
2022-08-08 17:42:03 +0700

    Add debug activity

--- app/src/main/java/com/example/deafable/MainActivity.java
+++ app/src/main/java/com/example/deafable/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.deafable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] mobileArray = {"Splash Screen","Onboarding"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.activity_listview, mobileArray);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, mobileArray[i], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
