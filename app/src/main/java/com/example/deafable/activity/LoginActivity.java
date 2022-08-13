54d493347f3faafbb695be17cc1cbe8ac84fad07
Moanrisy <moanrisy@gmail.com>
2022-08-13 12:08:16 +0700

    Remove action bar

--- app/src/main/java/com/example/deafable/activity/LoginActivity.java
+++ app/src/main/java/com/example/deafable/activity/LoginActivity.java
@@ -4,18 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.deafable.R;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
     private View parent_view;
+    private TextInputEditText ti_email;
+    private TextInputEditText ti_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         parent_view = findViewById(android.R.id.content);
+        ti_email = (TextInputEditText) findViewById(R.id.ti_email);
+        ti_password = (TextInputEditText) findViewById(R.id.ti_password);
+
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
 
         ((View) findViewById(R.id.forgot_password)).setOnClickListener(new View.OnClickListener() {
             @Override
